d7116f6043a66193cfa1f6b578d272c79d35dc94
Malaxor <nogobaduk@gmail.com>
2021-07-22 21:34:59 -0400

    game ready

--- app.js
+++ app.js
@@ -2,249 +2,139 @@ const cowEl = document.getElementById('cow');
 const grassEl = document.querySelector('#grass');
 const directionsDiv = document.querySelector('#directions');
 const keysDisplayEl = document.getElementById('directions__key-display');
+const alertDiv = document.getElementsByClassName('alert')[0];
 let moveBy = 10; // move the cow 10 pixels (up, down, left or right).
+const { height: cowElHeight } = window.getComputedStyle(cowEl);
+let { height: grassElHeight, width: grassElWidth } = window.getComputedStyle(grassEl);
 
-(function cowStartPosition() {
-   let { height, width } = window.getComputedStyle(grassEl);
+// cow's start position predicated on the field's length and width divided by two
+function cowStartPosition({ height, width }) {
    height = parseInt(height) / 2;
    width = parseInt(width) / 2;
-   
+
    cowEl.style.position = 'absolute';
    cowEl.style.left = `${width}px`;
    cowEl.style.top = `${height}px`;
-})();
-
-// window.addEventListener('DOMContentLoaded', () => {
-//    cowStartPosition();
-// });
+}
+// load cow's start position
+cowStartPosition(window.getComputedStyle(grassEl))
 // resets the cow's position but not it's rotation
-// window.addEventListener('resize', () => cowStartPosition());
-
+window.addEventListener('resize', () => {
+   cowStartPosition(window.getComputedStyle(grassEl));
+   window.getComputedStyle(grassEl);
+   cowEl.getBoundingClientRect();
+});
 
 // show error when user gives cow more than 10 directions
 function showError(error) {
-   const errorDiv = document.createElement('div');
-   errorDiv.className = 'alert';
-   errorDiv.append(error);
-   const { textContent } = document.querySelector('#directions__key-display');
-
-   document.querySelector('#directions__key-display').remove();
-   document.getElementById('directions').append(errorDiv);
-
-   return function() {
-      setTimeout(() => {
-         document.querySelector('.alert').remove();
-         const keyDisplay = document.createElement('p');
-         keyDisplay.id = 'directions__key-display';
-         keyDisplay.textContent = textContent;
-         directionsDiv.append(keyDisplay);
-      }, 5000)
-   }
-
-   // hide the paragraph displaying the user's key presses
-   // it exists in the DOM but doesn't take up space
-   // keyDisplay.style.display = 'none';
-   // the errorDiv will, in a sense, replace the hidden keyDisplay paragraph
-   // directionsDiv.append(errorDiv);
-   // after 5 seconds, we'll remove the erroDiv and show the keyDisplay paragraph 
-   setTimeout(clearError, 5000);
+   // hide the user's keyboard input
+   keysDisplayEl.style.display = 'none';
+   alertDiv.textContent = error;
+   // reveal the alert div and its text content
+   alertDiv.style.display = 'block';
+   setTimeout(clearError, 5000)
 }
-// clear error
-// function clearError() {
-//    document.querySelector('.alert').remove();
-//    const keyDisplay = document.createElement('p');
-//    keyDisplay.className = 'directions__key-display';
-//    keyDisplay.style.textContent = keysDisplayEl.style.textContent;
-//    directionsDiv.append(keyDisplay);
+function clearError() {
+   alertDiv.style.display = 'none';
+   keysDisplayEl.style.display = 'block';
+}
 
-   // keyDisplay.style.display = 'block';
-// }
-// convert keyboard arrow keys to cardinal directions
-const cardinalDir = {
-   ArrowUp: 'North',
-   ArrowDown: 'South',
-   ArrowRight: 'East',
-   ArrowLeft: 'West'
-};
-const cow = {
-   directions: ['North'],
-   addDirection(direction) {
-      this.directions.push(direction);
-   }
-};
+let degrees = 0; // will increment or decrement by 90 or 180
+let direction = null; // keep track of the direction (key arrows) to determine how much to increment or decrement
 window.addEventListener('keydown', e => {
-   if(e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
-      if(cow.directions.length < 10) {
-         keysDisplayEl.append(`${cardinalDir[e.key]} | `);
-         cow.addDirection(cardinalDir[e.key]);
-      }
-      else {
-         return showError("I'd love to stay and chat, but I'm hungry. Press Enter to mooooove me.")();
-      }
+   const { right: cowElRight } = cowEl.getBoundingClientRect();
+
+   switch(e.key) {
+      case 'ArrowUp':
+         if(!direction) {
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'East') {
+            degrees -= 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'West') {
+            degrees += 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'South') {
+            degrees -= 180;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         direction = 'North';
+         // code inside won't fire when the cow arrives at the field's North edge
+         if(parseInt(cowEl.style.top) > 5) {
+            cowEl.style.top = parseInt(cowEl.style.top) - moveBy + 'px';
+         }
+         else {
+            showError("I don't believe in Santa Clause.");
+         }
+      break;
+      case 'ArrowRight':
+         if(!direction || direction === 'North') {
+            degrees += 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'South') {
+            degrees -= 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'West') {
+            degrees -= 180;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         direction = 'East';
+         // code inside won't fire when the cow arrives at the field's East edge
+         if(cowElRight + 3 < parseInt(grassElWidth)) {
+            cowEl.style.left = parseInt(cowEl.style.left) + moveBy + 'px';
+         }
+         else {
+            showError("The east coast is not my thing, chief.");
+         }
+      break;
+      case 'ArrowDown':
+         if(!direction || direction === 'North') {
+            degrees += 180;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'East') {
+            degrees += 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }   
+         else if(direction === 'West') {
+            degrees -= 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         direction = 'South';
+         // this formula ensures the cow stops a few pixels before the field's South edge
+         if(parseInt(cowEl.style.top) < (parseInt(grassElHeight) - parseInt(cowElHeight) - 5)) {
+            cowEl.style.top = parseInt(cowEl.style.top) + moveBy + 'px';
+         }
+         else {
+            showError("It's too hot down south.")
+         }
+      break;
+      case 'ArrowLeft':
+         if(!direction || direction === 'North') {
+            degrees -= 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'South') {
+            degrees += 90;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         else if(direction === 'East') {
+            degrees += 180;
+            cowEl.style.transform = `rotate(${degrees}deg)`;
+         }
+         direction = 'West'; // West
+         // this formula ensures the cow stops a few pixels before the field's West edge
+         if(parseInt(cowEl.style.left) > 5) {
+            cowEl.style.left = parseInt(cowEl.style.left) - moveBy + 'px';
+         }
+         else {
+            showError("I can't afford California!");
+         }
+      break;
    }
 });
-
-let num = 0; // will increment or decrement by 90 or 180
-let key = null; // keep track of keys to determine how much to increment or decrement
-cow.directions.forEach(direction => {
-   switch(direction) {
-      case 'North':
-         if(!key) {
-            cowEl.style.transform = 'rotate(0deg)';
-         }
-         else if(key === 'East') {
-            num -= 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'West') {
-            num += 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'South') {
-            num -= 180;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         key = direction; // North
-         setTimeout(() => cowEl.style.top = parseInt(cowEl.style.top) - moveBy + 'px', 1000);
-         // cowEl.style.top = parseInt(cowEl.style.top) - moveBy + 'px';
-      break;
-
-      case 'East':
-         if(!key || key === 'North') {
-            num += 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'South') {
-            num -= 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'West') {
-            num -= 180;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         key = direction; // East
-         setTimeout(() => cowEl.style.left = parseInt(cowEl.style.left) + moveBy + 'px', 2000);
-         // cowEl.style.left = parseInt(cowEl.style.left) + moveBy + 'px';
-      break;
-
-      case 'South':
-         if(!key || key === 'North') {
-            num += 180;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'East') {
-            num += 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }   
-         else if(key === 'West') {
-            num -= 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         key = direction; // South
-         cowEl.style.top = parseInt(cowEl.style.top) + moveBy + 'px';
-      break;
-
-      case 'West':
-         if(!key || key === 'North') {
-            num -= 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'South') {
-            num += 90;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         else if(key === 'East') {
-            num += 180;
-            cowEl.style.transform = `rotate(${num}deg)`;
-         }
-         key = direction; // West
-         cowEl.style.left = parseInt(cowEl.style.left) - moveBy + 'px';
-      break;
-   }   
-});
-
-
-
-
-
-// let num = 0; // will increment or decrement by 90 or 180
-// let key = null; // keep track of keys to determine how much to increment or decrement
-// window.addEventListener('keydown', e => {
-//    switch(e.key) {
-//       case 'ArrowUp':
-//          if(!key) {
-//             cow.style.transform = 'rotate(0deg)';
-//          }
-//          else if(key === 'ArrowRight') {
-//             num -= 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowLeft') {
-//             num += 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowDown') {
-//             num -= 180;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          key = e.key; // ArrowUp
-//          keyDisplay.append('North | ');
-//          // ex: parseInt('0deg') returns the number 0
-//          cow.style.top = parseInt(cow.style.top) - moveBy + 'px';
-//       break;
-
-//       case 'ArrowRight':
-//          if(!key || key === 'ArrowUp') {
-//             num += 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowDown') {
-//             num -= 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowLeft') {
-//             num -= 180;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          key = e.key; // ArrowRight
-//          keyDisplay.append('East | ');
-//          cow.style.left = parseInt(cow.style.left) + moveBy + 'px';
-//       break;
-      
-//       case 'ArrowDown':
-//          if(!key || key === 'ArrowUp') {
-//             num += 180;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowRight') {
-//             num += 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }   
-//          else if(key === 'ArrowLeft') {
-//             num -= 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          key = e.key; // ArrowDown
-//          keyDisplay.append('South | ');
-//          cow.style.top = parseInt(cow.style.top) + moveBy + 'px';
-//       break;
-
-//       case 'ArrowLeft':
-//          if(!key || key === 'ArrowUp') {
-//             num -= 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowDown') {
-//             num += 90;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          else if(key === 'ArrowRight') {
-//             num += 180;
-//             cow.style.transform = `rotate(${num}deg)`;
-//          }
-//          key = e.key; // ArrowLeft
-//          keyDisplay.append('West | ');
-//          cow.style.left = parseInt(cow.style.left) - moveBy + 'px';
-//       break;
-//    }
-// });
